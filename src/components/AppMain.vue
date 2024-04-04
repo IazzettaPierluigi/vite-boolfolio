498e55c12c91983f0f41f37cf826b84a25adc7f7
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-04-04 19:52:12 +0200

    show with projectcard component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,19 +1,29 @@
 <script>
 import axios from 'axios';
 
+import ProjectCard from './ProjectCard.vue'
+
 export default {
   name: "AppMain",
 
+  components: {
+    ProjectCard
+  },
+
   data() {
     return {
-
+      arrayProjects: []
     }
   },
 
   methods: {
     getProjects() {
       axios.get('http://127.0.0.1:8000/api/test').then(res => {
+
         console.log(res.data.projects)
+
+        this.arrayProjects = res.data.projects
+
       })
     }
   },
@@ -28,9 +38,18 @@ export default {
 
 <template>
   <main>
-    <p>projects</p>
+
+    <div v-for="project in arrayProjects" :key="project.id">
+      <ProjectCard :project="project" />
+    </div>
   </main>
 
 </template>
 
-<style scoped></style>
+<style scoped>
+main {
+  display: flex;
+  flex-direction: row;
+  gap: 50px;
+}
+</style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="project-card">
+        <h2>{{ project.title }}</h2>
+        <img :src="project.img" alt="Project Image">
+        <p>descrizione progetto: <span style="color: red;">{{ project.description }}</span></p>
+        <!-- Aggiungi qui qualsiasi altra informazione del progetto che vuoi visualizzare -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        project: {
+            type: Object,
+            required: true
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.project-card {
+    border: 1px solid grey;
+    width: 300px;
+    text-align: center;
+}
+</style>
