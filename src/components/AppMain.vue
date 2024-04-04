d77bca6e9c46f770a2e68fd5b48969bab22a1b93
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-04-04 19:12:54 +0200

    cleaning app vue and main app

---
+++ src/components/AppMain.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: "AppMain",
+
+  data() {
+    return {
+
+    }
+  },
+
+  methods: {
+
+  },
+
+  mounted() {
+
+  }
+
+
+}
+</script>
+
+<template>
+  <main>
+    <p>projects</p>
+  </main>
+
+</template>
+
+<style scoped></style>
