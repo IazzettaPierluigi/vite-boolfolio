dff4d8a6825127e23111ef53498e3c414b100ec2
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-04-08 17:25:14 +0200

    router and components

--- src/router.js
+++ src/router.js
@@ -1,1 +1,25 @@
 import { createRouter, createWebHistory } from "vue-router";
+
+import HomePage from './pages/HomePage.vue';
+
+import ProjectList from './pages/ProjectList.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomePage
+        },
+
+        {
+            path: '/blog',
+            name: 'blog',
+            component: ProjectList
+        }
+    ]
+});
+
+export { router };
