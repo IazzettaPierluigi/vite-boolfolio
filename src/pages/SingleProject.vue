91dafb043b234d9758aae65da512caa932fe9ed4
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-04-09 16:27:53 +0200

    singleproject show

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -9,17 +9,25 @@ export default {
 
     data() {
         return {
-
+            project: ''
         }
     },
 
     methods: {
+        getSingleProject() {
+            axios.get(`http://127.0.0.1:8000/api/test/${this.$route.params.slug}`).then(res => {
+                if (res.data.success) {
+                    this.project = res.data.project
+                } else {
 
+                }
+            })
+        }
 
     },
 
     mounted() {
-
+        this.getSingleProject()
     }
 
 
@@ -27,7 +35,21 @@ export default {
 </script>
 
 <template>
-    <h1>Single project</h1>
+    <div class="container">
+        <div class="row justify-content-center ">
+            <div class="card col-3">
+                <img class="card-img-top" :src="`http://127.0.0.1:8000/storage/${project.img}`" alt="Title" />
+                <div class="card-body">
+                    <h4 class="card-title">{{ project.title }}</h4>
+                    <p class="card-text">{{ project.description }}</p>
+                    <p class="card-tex">{{ project.technology }}</p>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
 
 </template>
 
